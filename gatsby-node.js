3b9d14df3cec73be4b1d5a43892b2da161ae7397
anne k <50986817+nefarious-onion@users.noreply.github.com>
2020-08-09 12:42:35 +0300

    initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,10 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+//For absolute imports
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+        resolve: {
+            modules: [path.resolve(__dirname, "src"), "node_modules"]
+        }
+    })
+}
